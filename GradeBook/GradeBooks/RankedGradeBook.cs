8e2acaab57b1de78be2b3f471af059af0835fa07
jordanabawi <jordan.abawi@aruplab.com>
2018-12-17 11:39:43 -0700

    module 3 update
    Completed the Module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -7,9 +8,41 @@ namespace GradeBook.GradeBooks
 {
     class RankedGradeBook : BaseGradeBook
     {
-        RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var studentThreshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var students = Students.OrderByDescending(s => s.AverageGrade).ToArray();
+            if (averageGrade >= students[(studentThreshold)-1 ].AverageGrade)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= students[(studentThreshold*2) -1].AverageGrade)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= students[(studentThreshold*3)-1].AverageGrade)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= students[(studentThreshold*4) -1].AverageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -14,38 +14,6 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Standard;
         }
 
-        public override char GetLetterGrade(double averageGrade)
-        {
-            char grade;
-            if (this.Students.Count < 5)
-            {
-                throw new InvalidOperationException();
-            }
-
-            var studentThreshold = (int) Math.Ceiling(this.Students.Count * 0.2);
-            var students = Students.OrderBy(s => s.AverageGrade).ToList();
-            if (averageGrade >= students.ElementAt(studentThreshold * 4).AverageGrade)
-            {
-                grade= 'A';
-            }
-            else if (averageGrade >= students.ElementAt(studentThreshold * 3).AverageGrade)
-            {
-                grade = 'B';
-            }
-            else if (averageGrade >= students.ElementAt(studentThreshold * 2).AverageGrade)
-            {
-                grade = 'C';
-            }
-            else if (averageGrade >= students.ElementAt(studentThreshold ).AverageGrade)
-            {
-                grade = 'D';
-            }
-            else
-            {
-                grade = 'F';
-            }
-
-            return grade;
-        }
+        
     }
 }
